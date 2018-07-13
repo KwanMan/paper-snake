ba5c0b56673a494e19e2de49d826c427f68c541e
Tommy Kwan <tkwan196@gmail.com>
2018-07-13 13:50:53 +0100

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/start.js',
+  output: {
+    file: 'dist/paper-snake.js',
+    format: 'cjs'
+  },
+  plugins: [
+    babel({
+      babelrc: false,
+      presets: [['babel-preset-env', { modules: false }]],
+      plugins: ['babel-plugin-transform-object-rest-spread']
+    })
+  ]
+}
