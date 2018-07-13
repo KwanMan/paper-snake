ffdf7dc6ec979902a598fadfe64e700f7553c526
Tommy Kwan <tkwan196@gmail.com>
2018-07-13 12:39:07 +0100

    Bundle everything

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+module.exports = {
+  mode: 'none',
+  entry: './src/start.js',
+  output: {
+    filename: 'paper-snake.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: modulePath => {
+          return (
+            /node_modules/.test(modulePath) &&
+            !/node_modules\/roughjs/.test(modulePath)
+          )
+        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+            plugins: ['babel-plugin-transform-object-rest-spread'],
+            babelrc: false
+          }
+        }
+      }
+    ]
+  }
+}
